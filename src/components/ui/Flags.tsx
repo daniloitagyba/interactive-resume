25c6969ecb8704cd856851ecbccbb561c280addc
Danilo Itagyba <daniloitagyba@gmail.com>
2026-02-11 18:44:13 -0300

    fix: update US and BR flag SVG components for improved rendering and consistency

--- src/components/ui/Flags.tsx
+++ src/components/ui/Flags.tsx
@@ -1,40 +1,33 @@
 import React from 'react';
 
 export const FlagUS: React.FC<{ className?: string }> = ({ className }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 480" className={className}>
-    <g fillRule="evenodd">
-      <path fill="#bd3d44" d="M0 0h640v480H0"/>
-      <path fill="#fff" d="M0 55h640v37H0zm0 74h640v37H0zm0 74h640v37H0zm0 74h640v37H0zm0 74h640v37H0zm0 74h640v37H0z"/>
-      <path fill="#192f5d" d="M0 0h296v259H0z"/>
-      <g fill="#fff">
-        <g id="s18">
-          <g id="s9">
-            <g id="s5">
-              <g id="s4">
-                <path id="s" d="M247 90l5 15.2h16l-13 9 5 15.2-13-9-13 9 5-15.2-13-9h16z"/>
-                <use xlinkHref="#s" y="42"/>
-                <use xlinkHref="#s" y="84"/>
-                <use xlinkHref="#s" y="126"/>
-              </g>
-              <use xlinkHref="#s" y="168"/>
-            </g>
-            <use xlinkHref="#s4" x="247" y="21"/>
-          </g>
-          <use xlinkHref="#s9" x="-247"/>
-        </g>
-        <use xlinkHref="#s18" x="-50"/>
-        <use xlinkHref="#s9" x="-100"/>
-        <use xlinkHref="#s5" x="-150"/>
+  <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" className={className}>
+    <mask id="circleMaskUS">
+      <circle cx="256" cy="256" r="256" fill="white" />
+    </mask>
+    <g mask="url(#circleMaskUS)">
+      <rect width="512" height="512" fill="#f0f0f0" />
+      <path d="M0 36h512v72H0zm0 144h512v72H0zm0 144h512v72H0zm0 144h512v72H0z" fill="#d80027" />
+      <rect width="270" height="270" fill="#2e52b2" />
+      <g fill="white">
+        <circle cx="65" cy="65" r="22" /><circle cx="135" cy="65" r="22" /><circle cx="205" cy="65" r="22" />
+        <circle cx="65" cy="135" r="22" /><circle cx="135" cy="135" r="22" /><circle cx="205" cy="135" r="22" />
+        <circle cx="65" cy="205" r="22" /><circle cx="135" cy="205" r="22" /><circle cx="205" cy="205" r="22" />
       </g>
     </g>
   </svg>
 );
 
 export const FlagBR: React.FC<{ className?: string }> = ({ className }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 480" className={className}>
-    <path fill="#009c3b" d="M0 0h640v480H0z"/>
-    <path fill="#ffdf00" d="m320 40 280 200-280 200L40 240z"/>
-    <circle cx="320" cy="240" r="140" fill="#002776"/>
-    <path fill="#fff" d="M180 240a140 140 0 0 0 280 0c0-10-8-10-8-10a140 140 0 0 1-264 10s-8 0-8 10z"/>
+  <svg viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg" className={className}>
+    <mask id="circleMaskBR">
+      <circle cx="256" cy="256" r="256" fill="white" />
+    </mask>
+    <g mask="url(#circleMaskBR)">
+      <rect width="512" height="512" fill="#009c3b" />
+      <path d="M256 60l188 196-188 196-188-196z" fill="#ffdf00" />
+      <circle cx="256" cy="256" r="110" fill="#002776" />
+      <path d="M160 265c40-15 110-15 192 0" fill="none" stroke="white" strokeWidth="18" strokeLinecap="round" />
+    </g>
   </svg>
 );
